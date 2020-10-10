1901b9d77e85dd82a52ee66ce3b4379c22bd6c61
Aksel Stadler <aksel@stadler.no>
2020-10-10 11:35:41 +0200

    deploy

--- apps/proxy/config/prod.exs
+++ apps/proxy/config/prod.exs
@@ -1,10 +1,12 @@
 use Mix.Config
 
+
 config :master_proxy, 
   # any Cowboy options are allowed
   #http: [:inet6, port: 80],
-  #http: [:inet6, port: 4000],
-  http: [port: {:system, "PORT"}],  backends: [
+  http: [:inet6, port: 4000],
+#  http: [port: {:system, "PORT"}],
+backends: [
     %{
       host: ~r/prova.stadler.no/,
      phoenix_endpoint: ProvaNoWeb.Endpoint
