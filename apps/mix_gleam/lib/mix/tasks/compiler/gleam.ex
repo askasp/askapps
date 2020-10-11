b31ac37ba6e0b1a6d440e2694c485c5747fbe4e9
Aksel Stadler <aksel@stadler.no>
2020-10-11 10:31:51 +0200

    deploy

--- apps/mix_gleam/lib/mix/tasks/compiler/gleam.ex
+++ apps/mix_gleam/lib/mix/tasks/compiler/gleam.ex
@@ -3,7 +3,7 @@
   use Mix.Task.Compiler
 
   def run(_args) do
-    case Mix.shell().cmd("../mix_gleam/gleam build .") do
+    case Mix.shell().cmd("apps/mix_gleam/gleam build .") do
       0 -> {:ok, []}
       status -> exit(status)
     end
