278f9fad8d1657ebd866d53def6458287785478a
Aksel Stadler <aksel@stadler.no>
2020-10-25 11:06:38 +0100

    new change

--- apps/stadler_no/mix.exs
+++ apps/stadler_no/mix.exs
@@ -4,7 +4,7 @@ defmodule StadlerNo.MixProject do
   def project do
     [
       app: :stadler_no,
-      version: "0.1.0",
+      version: "0.1.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- rel/config.exs
+++ rel/config.exs
@@ -47,7 +47,7 @@ end
 # will be used by default
 
 release :askapps do
-  set version: "0.1.3"
+  set version: "0.1.4"
   set applications: [
     :runtime_tools,
     :os_mon,
