8148591b9dd4235f76bce22446db43a241fe5cfe
aksel <aksel@stadler.no>
2020-12-23 13:06:36 +0100

    fix live analytics

--- apps/stadler_no/startserves.sh
+++ apps/stadler_no/startserves.sh
@@ -1,6 +1,5 @@
 
 export GCP_CREDENTIALS=$(cat /home/ask/git/.secrets/askapps-7b6e83304aa0.json)
-export BUCKET=stadler_no_test
-export IPINFO_API_KEY=9218470f478ec9
+export BUCKET=stadler_no
 mix deps.get
 iex -S mix phx.server
