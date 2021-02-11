ad7b76be32c3b0db0471002d7645c5d0268531f9
Aksel Stadler <aksel@stadler.no>
2021-02-11 18:27:45 +0100

    deploy

--- apps/stadler_no/lib/stadler_no_web/router.ex
+++ apps/stadler_no/lib/stadler_no_web/router.ex
@@ -10,7 +10,7 @@ defmodule StadlerNoWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :log
-    plug LiveAnalytics.AsyncLogRequest
+#    plug LiveAnalytics.AsyncLogRequest
   end
 
   pipeline :api do
